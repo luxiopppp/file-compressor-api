9ff3af1ecabec57cddb64c33f66dbf3bc5dff7c8
lucio <102931813+luxiopppp@users.noreply.github.com>
2025-02-25 00:44:07 -0300

    feat: add multiple file upload support

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require( 'dotenv' ).config();
+const path = require('path')
 
 const upload = require('./utils/upload')
 const { compressImage, compressVideo } = require('./utils/compress')
@@ -12,27 +13,31 @@ app.get('/', (req, res) => {
   res.send('API running!')
 })
 
-app.post('/compress', upload.single('file'), async (req, res) => {
-  if (!req.file) {
+app.post('/compress', upload.array('files'), async (req, res) => {
+  if (!req.files) {
     return res.status(400).json({ error: "No se ha subido ningún archivo" });
   }
 
-  console.log("Archivo recibido:", req.file);
-
-  const mimeType = req.file.mimetype;
-  let returnFile;
+  console.log("Archivos recibidos:", req.files);
 
   try {
-    if (mimeType.startsWith('image/')) {
-      returnFile = await compressImage(req.file.originalname)
-    } else {
-      returnFile = await compressVideo(req.file.originalname)
-    }
+    const compressedFiles = await Promise.all(req.files.map(async ( file ) => {
+      const mimeType = file.mimetype;
+      let returnFile;
+
+      if (mimeType.startsWith('image/')) {
+        returnFile = await compressImage(file.originalname);
+      } else if (mimeType.startsWith('video/')) {
+        returnFile = await compressVideo(file.originalname);
+      }
+    
+      return returnFile;
+    }));
+
+    res.status(200).send(compressedFiles)
   } catch (err) {
     res.status(500).send('Error en la compresión del archivo.')
   }
-  
-  res.status(200).download(returnFile);
 });
 
 const PORT = process.env.PORT || 8080;
